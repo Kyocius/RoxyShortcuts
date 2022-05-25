679131f44e587f6f3deb652616a165891ac0cdbe
Yoroion <1738435753@qq.com>
2022-05-25 17:24:35 +0800

    Initialize Project

---
+++ Roxy/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Roxy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += (_, _) =>
+            {
+                WPFUI.Appearance.Watcher.Watch(
+                    this, // Window class
+                    WPFUI.Appearance.BackgroundType.Mica, // Background type
+                    true // Whether to change accents automatically
+                );
+            };
+        }
+    }
+}
